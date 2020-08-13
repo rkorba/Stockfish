eefcff40002110d50a526800cd66209980687519
Roman Korba <korba.roman@gmail.com>
2020-08-13 18:45:18 +0200

    use R=4 in iid if PV

--- src/search.cpp
+++ src/search.cpp
@@ -944,13 +944,16 @@ namespace {
     }
 
     // Step 11. Internal iterative deepening (~1 Elo)
-    if (depth >= 7 && !ttMove)
     {
-        search<NT>(pos, ss, alpha, beta, depth - 7, cutNode);
+        const int iidDepth = NT == PV ? depth-4 : depth-7;
+        if (iidDepth >= 1 && !ttMove)
+        {
+            search<NT>(pos, ss, alpha, beta, iidDepth, cutNode);
 
-        tte = TT.probe(posKey, ttHit);
-        ttValue = ttHit ? value_from_tt(tte->value(), ss->ply, pos.rule50_count()) : VALUE_NONE;
-        ttMove = ttHit ? tte->move() : MOVE_NONE;
+            tte = TT.probe(posKey, ttHit);
+            ttValue = ttHit ? value_from_tt(tte->value(), ss->ply, pos.rule50_count()) : VALUE_NONE;
+            ttMove = ttHit ? tte->move() : MOVE_NONE;
+        }
     }
 
 moves_loop: // When in check, search starts from here
